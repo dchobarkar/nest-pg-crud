ff79a1e135d9c1421654a3a9141f980a46047705
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-12 15:24:16 +0530

    create cities database

--- src/cities/entities/city.entity.ts
+++ src/cities/entities/city.entity.ts
@@ -1,1 +1,16 @@
-export class City {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'cities' })
+export class City {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ type: 'boolean', default: true })
+  active: boolean;
+}
